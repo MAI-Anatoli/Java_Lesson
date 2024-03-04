72c7fa131d795fbfc56f085571c7ffa790f40cd1
Anatoli Marian <bmwm5f15.tm@gmail.com>
2024-03-04 07:18:02 +0200

    Lektii and Seminar

---
+++ Lektii/Lektii01/program.java
@@ -0,0 +1,21 @@
+package Lektii;
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (i % 2 != 0){
+        continue;
+    }
+		System.out.println(i);
+}
+
+
+	}
+}
+
+ 
---
+++ Seminar/Seminar01/Seminar_lesson01.java
@@ -0,0 +1,7 @@
+package Seminar.Seminar01;
+
+public class Seminar_lesson01 {
+	public static void main(String[] args) {
+
+	}
+}
