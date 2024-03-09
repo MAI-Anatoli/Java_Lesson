b55e0c0c0bead1e1031b4f65c8fbaccbd81e7516
MAI-Anatoli <67876357+MAI-Anatoli@users.noreply.github.com>
2024-03-09 14:08:29 +0200

    Seminar01

--- Seminar/Seminar01/src/task04.java
+++ Seminar/Seminar01/src/task04.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class task04 {
+
+/*Реализовать функцию возведения числа а в степень b. a, b из Z. Сводя количество выполняемых действий к минимуму.
+Пример 1: а = 3, b = 2, ответ: 9
+Пример 2: а = 2, b = -2, ответ: 0.25
+Пример 3: а = 3, b = 0, ответ: 1*/
+public class task04 {
+    public static void main(String[] args) {
+
+ System.out.println(pow(3, 2));
+        System.out.println(pow(2, -2));
+        System.out.println(pow(3, 0));
+        System.out.println(pow(0, 100));
+        System.out.println(pow(1, 100));
+    }
+
+    static double pow(double num1, int num2) {
+        double res = 1;
+        if (num1 == 0 || num1 == 1) return num1;
+        if (num2 < 0){
+            num1 = 1 / num1;
+            num2 = -num2;
+        }
+        for (int i = 0; i < num2; i++) {
+            res *= num1;
+        }
+        return res;
+    }
 }
